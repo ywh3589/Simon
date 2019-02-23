08f46defeb784f650b4dfa6186e24ab0811ff129
Dakota Backus <backusdakota@gmail.com>
2019-02-22 17:16:46 -0800

    Updated list of commands

--- command.c
+++ command.c
@@ -5,10 +5,14 @@
 int main(int argc, char *argv[])
 {
   printf(1, "\n");
+  printf(1, "NOTE: [] -> optional parameter, () -> required parameter\n\n");
+  printf(1, "command      -  will display a list value shell commands\n");
   printf(1, "echo [text]  -  will output the text that follows the command\n");
-  printf(1, "ps           -  will display a list of running processes\n");
+  printf(1, "ps [-l]      -  will display a list of running processes\n");
   printf(1, "ls           -  will display a list of files in the current directory\n");
   printf(1, "clear        -  will clear the terminal of its current text\n");
+  printf(1, "renice (nice) (pid) (priority)\n");
+  printf(1, "             -  will change priority of a currently executing process\n");
   printf(1, "\n");
 
   exit();
